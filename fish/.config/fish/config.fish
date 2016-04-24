864c70c93cfd602d2a68d2018b3dab4b3f83c62a
Ondřej Svoboda <theodik@gmail.com>
2016-04-24 18:00:46 +0200

    fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,39 +1,44 @@
 if status --is-login
-	for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin /opt/local/lib/postgresql83/bin ~/bin ~/.config/fish/bin 
-		if test -d $p
-			set PATH $p $PATH
-		end
-	end
+  for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin /opt/local/lib/postgresql83/bin ~/bin ~/.config/fish/bin 
+    if test -d $p
+      set PATH $p $PATH
+    end
+  end
 end
 
 
 
 set fish_greeting ""
 set -x CLICOLOR 1
+set -x EDITOR "vim"
+
+# set prompt color
+set -g fish_color_user 0087ff
+set -g fish_color_status red
 
 function parse_git_branch
-	sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+  sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 end
 
 function parse_git_tag
-	git describe --tags --always ^/dev/null
+  git describe --tags --always ^/dev/null
 end
 
 function parse_git_tag_or_branch
-	if [ (parse_git_branch) != "(no branch)" ]
-        	parse_git_branch
-	else
-        	parse_git_tag
-	end
+  if [ (parse_git_branch) != "(no branch)" ]
+          parse_git_branch
+  else
+          parse_git_tag
+  end
 end
 
 function git_parse_ahead_of_remote
-	git status ^/dev/null | grep 'Your branch is ahead of' | sed -e 's/# Your branch is ahead of .* by \(.*\) commit.*/\1/g'
+  git status ^/dev/null | grep 'Your branch is ahead of' | sed -e 's/# Your branch is ahead of .* by \(.*\) commit.*/\1/g'
 end
 
 function is_git
-	git status >/dev/null ^&1	
-	return $status
+  git status >/dev/null ^&1 
+  return $status
 end
 
 function parse_svn_tag_or_branch
@@ -41,54 +46,26 @@ function parse_svn_tag_or_branch
 end
 
 function parse_svn_revision
-	sh -c 'svn info 2> /dev/null' | sed -n '/^Revision/p' | sed -e 's/^Revision: \(.*\)/\1/'
+  sh -c 'svn info 2> /dev/null' | sed -n '/^Revision/p' | sed -e 's/^Revision: \(.*\)/\1/'
 end
 
 function is_svn
-	svnversion | egrep -vq '^(Unversioned directory|exported)'
-	return $status
+  if not which svnversion ^/dev/null
+    return 1
+  end
+
+  svnversion | egrep -vq '^(Unversioned directory|exported)'
+  return $status
 end
-
-function fish_prompt -d "Write out the prompt"
-	printf '%s%s@%s%s' (set_color brown) (whoami) (hostname|cut -d . -f 1) (set_color normal) 
-
-	# Color writeable dirs green, read-only dirs red
-	if test -w "."
-		printf ' %s%s' (set_color green) (prompt_pwd)
-	else
-		printf ' %s%s' (set_color red) (prompt_pwd)
-	end
-
-        # Print subversion tag or branch
-	if is_svn
-                printf ' %s%s%s' (set_color normal) (set_color blue) (parse_svn_tag_or_branch)
-        end
-        
-	# Print subversion revision
-	if is_svn
-		printf '%s%s@%s' (set_color normal) (set_color blue) (parse_svn_revision)
-	end
-
-	# Print git tag or branch
-	if is_git
-		printf ' %s%s/%s' (set_color normal) (set_color blue) (parse_git_tag_or_branch)
-		set git_ahead_of_remote (git_parse_ahead_of_remote)
-		if [ -n "$git_ahead_of_remote" -a "$git_ahead_of_remote" != "0" ]
-			printf ' +%s' (git_parse_ahead_of_remote)
-		end
-	end
-	printf '%s> ' (set_color normal)
-end
-
 
 bind \cr "rake"
 
 function ss -d "Run the script/server"
-	script/server
+  script/server
 end
 
 function sc -d "Run the Rails console"
-	script/console
+  script/console
 end
 
 if test -d "/opt/java"
